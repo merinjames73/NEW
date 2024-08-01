65895d1e3f118e3de4ba36b56126b8ae61cafeef
merin james <137210095+merinjames73@users.noreply.github.com>
2024-08-02 04:51:52 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,7 @@
-// src/components/Navbar.js
+
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBell, faCog } from '@fortawesome/free-solid-svg-icons';
 import './Navbar.css';
 
 function Navbar() {
@@ -7,7 +9,8 @@ function Navbar() {
     <div className="navbar">
       <input type="text" placeholder="Search Here..." />
       <div className="navbar-icons">
-        <i className="fas fa-bell"></i>
+        <FontAwesomeIcon icon={faCog} className="icon" />
+        <FontAwesomeIcon icon={faBell} className="icon" />
         <img src="pic.avif" alt="Profile" />
       </div>
     </div>
